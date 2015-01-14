dc363fef8bff0ab3a71818e507060ad91522eca7
kalkin7 <kalkin7@gmail.com>
2015-01-14 10:39:59 +0900

    Initializing

--- git_blog_update.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal
-
-chcp 65001
-git pull origin gh-pages
-git add . -A
-git commit -m "Blog Update"
-git push origin gh-pages
--- git_link_update.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal
-
-chcp 65001
-git pull origin gh-pages
-git add . -A
-git commit -m "Link Page Update"
-git push origin gh-pages
